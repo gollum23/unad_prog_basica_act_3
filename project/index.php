2f9f1f33cf51c776df17cb32a932d6154421f89e
Diego Forero <gollum23@gmail.com>
2016-10-04 07:36:37 -0500

    First commit, Exercise 1

---
+++ project/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Diego Forero</title>
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/bootstrap-theme.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="page-header">
+                            <h1 class="text-center">Programación de Sitios Web Actividad 3</h1>
+                            <h4 class="text-center">Diego Alexander Forero Higuera</h4>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+            <div class="content">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 1</div>
+                    <div class="panel-body">
+                        <p>Crear un arreglo que almacene 5 números aleatorios y luego ordenarlos y mostrar el arreglo como queda ordenado.</p>
+                        <p><a href="ejercicio_1/">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 2</div>
+                    <div class="panel-body">
+                        <p>Almacenar en una matriz 10 nombres, luego de estar almacenados los 10 nombres, insertar un nuevo nombre al final y al inicio de la matriz, luego de
+                            tener los 12 nombres, mostrarlos, luego eliminar el nombre que está en la 5 posición de la matriz. Luego mostrar la nueva matriz,
+                            luego ordenarla y mostrarla.</p>
+                        <p><a href="ejercicio_2/">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 3</div>
+                    <div class="panel-body">
+                        <p>En un arreglo A almacenar 5 números, luego en un arreglo B copiar los números del arreglo A pero elevados al cuadrado (x^2),
+                            luego mostrar los dos arreglos.</p>
+                        <p><a href="ejercicio_3/">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 4</div>
+                    <div class="panel-body">
+                        <p>Capturar en un formulario la cedula, el nombre, apellido, edad, sexo, hobby y ciudad de residencia de una persona y almacenarlos en
+                            un archivo plano (.txt), también se debe tener la opción de mostrar los datos almacenados en el archivo plano.</p>
+                        <p><a href="ejercicio_4/">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 5</div>
+                    <div class="panel-body">
+                        <p>Generar una encuesta dinámica, que lo primero es crear una página para solicitar el número de personas, luego con este dato se
+                            genera la encuesta a cada pregunta, luego estos datos se envían y se generan los resultados.
+                            </p>
+                        <p>El tema de la encuesta será calidad de los acompañamientos síncronos y asíncronos en la UNAD</p>
+                        <p><a href="ejercicio_5/">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h2 class="text-center">Universidad Nacional Abierta y a Distancia</h2>
+                        <h4 class="text-center">Grupo 301127 - 13</h4>
+                        <p class="text-center">Bogotá, Colombia Octubre 2016</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+
+        <script src="./js/bootstrap.min.js"></script>
+    </body>
+</html>
